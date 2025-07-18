8a4cb3d7ea28e4f5a9bc846dafb25753863cf3fc
harshiltomar <harshiltomar20@gmail.com>
2025-07-18 21:49:21 +0530

    feat: mongodb init

---
+++ ai-assistant-backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// Load environment variables
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}).catch((err) => {
+  console.log("Error connecting to MongoDB", err);
+});
