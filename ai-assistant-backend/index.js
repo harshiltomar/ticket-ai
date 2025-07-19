46bb87ea92de90a9976f128c9110c01e3853731f
harshiltomar <harshiltomar20@gmail.com>
2025-07-19 22:32:08 +0530

    feat: index.js update

--- ai-assistant-backend/index.js
+++ ai-assistant-backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
+import { serve } from "inngest/express";
 import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
-// Load environment variables
+import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -14,12 +18,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}).catch((err) => {
-  console.log("Error connecting to MongoDB", err);
-});
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected ✅");
+    app.listen(PORT, () => console.log("🚀 Server at http://localhost:3000"));
+  })
+  .catch((err) => console.error("❌ MongoDB error: ", err));
